9772b556b197c0a713f00b86c5c78e8b0634693a
tchiotludo <tchiot.ludo@gmail.com>
2021-12-17 11:56:17 +0100

    chore(deps): update all java deps

--- src/test/java/io/kestra/plugin/aws/s3/AllTest.java
+++ src/test/java/io/kestra/plugin/aws/s3/AllTest.java
@@ -22,7 +22,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
 import java.util.Objects;
-import javax.inject.Inject;
+import jakarta.inject.Inject;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
