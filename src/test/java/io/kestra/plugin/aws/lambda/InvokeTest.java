f96e722d49d4e1290ab28c30f3b0f06aa5d77234
Peter Nedonosko <pnedonosko@gmail.com>
2023-09-18 16:10:50 +0300

    Improved Invoke error handling (show full error as is to the user); Added integration tests within LocalStack/Lambda (#240)

---
+++ src/test/java/io/kestra/plugin/aws/lambda/InvokeTest.java
@@ -0,0 +1,113 @@
+package io.kestra.plugin.aws.lambda;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import java.util.HashMap;
+import java.util.Map;
+import org.apache.http.entity.ContentType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import io.kestra.core.exceptions.IllegalVariableEvaluationException;
+import io.kestra.core.runners.RunContext;
+
+public class InvokeTest extends AbstractInvokeTest {
+
+    private RunContext context;
+
+    @BeforeEach
+    public void setUp() throws IllegalVariableEvaluationException {
+        this.context = runContextFactory.of();
+    }
+
+    @Test
+    public void givenExistingLambda_whenInvoked_thenOutputOkMetricsOk() throws Exception {
+        // Given
+        var invoke = Invoke.builder()
+                .endpointOverride(localstack.getEndpointOverride(LocalStackContainer.Service.LAMBDA)
+                        .toString())
+                .functionArn(FUNCTION_NAME)
+                .id(InvokeTest.class.getSimpleName())
+                .type(InvokeTest.class.getName())
+                .region(localstack.getRegion())
+                .accessKeyId(localstack.getAccessKey())
+                .secretKeyId(localstack.getSecretKey())
+                .build();
+
+        var client = invoke.client(context);
+        createFunction(client);
+
+        // When
+        var output = invoke.run(context);
+
+        // Then
+        assertNotNull(output.getUri(), "File URI should be present");
+        assertEquals(ContentType.APPLICATION_JSON.getMimeType(), output.getContentType(),
+                "Output content type should be present");
+        assertTrue(output.getContentLength() > 10, "Output content length should have a value");
+        assertTrue(
+                context.metrics().stream().filter(m -> m.getName().equals("file.size"))
+                        .map(m -> m.getValue()).findFirst().isPresent(),
+                "Metric file.size should be present");
+        assertTrue(
+                context.metrics().stream().filter(m -> m.getName().equals("duration"))
+                        .map(m -> m.getValue()).findFirst().isPresent(),
+                "Metric duration should be present");
+    }
+
+    @Test
+    public void givenNotFoundLambda_whenInvoked_thenErrorNoMetrics() throws Exception {
+        // Given
+        var invoke = Invoke.builder()
+                .endpointOverride(localstack.getEndpointOverride(LocalStackContainer.Service.LAMBDA)
+                        .toString())
+                .functionArn("Fake_ARN")
+                .id(InvokeTest.class.getSimpleName())
+                .type(InvokeTest.class.getName())
+                .region(localstack.getRegion())
+                .accessKeyId(localstack.getAccessKey())
+                .secretKeyId(localstack.getSecretKey())
+                .build();
+
+        var client = invoke.client(context);
+        createFunction(client);
+
+        // When
+        assertThrows(LambdaInvokeException.class, () -> invoke.run(context),
+                "Invokation should thrown an exception");
+
+        // Then
+        assertTrue(context.metrics().size() == 0, "Metrics should not be present");
+    }
+
+    @Test
+    public void givenFailingLambda_whenInvoked_thenFailureNoMetrics() throws Exception {
+        // Given
+        Map<String, Object> params = new HashMap<>();
+        // ask for an error in the Lambda by function param (see test resource lambda/test.py)
+        params.put("action", "error");
+        var invoke = Invoke.builder()
+                .endpointOverride(localstack.getEndpointOverride(LocalStackContainer.Service.LAMBDA)
+                        .toString())
+                .functionArn(FUNCTION_NAME).functionPayload(params)
+                .id(InvokeTest.class.getSimpleName())
+                .type(InvokeTest.class.getName())
+                .region(localstack.getRegion())
+                .accessKeyId(localstack.getAccessKey())
+                .secretKeyId(localstack.getSecretKey())
+                .build();
+
+        var client = invoke.client(context);
+        createFunction(client);
+
+        // When
+        assertThrows(LambdaInvokeException.class, () -> invoke.run(context),
+                "Invokation should fail");
+
+        // Then
+        assertTrue(context.metrics().size() == 0, "Metrics should not be present");
+    }
+
+}
---
+++ src/test/resources/lambda/test.py
@@ -0,0 +1,20 @@
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def handler(event, context):
+    print("Lambda function ARN:", context.invoked_function_arn)
+    action = event.get("action")
+    if action is not None:
+        if action == "error":
+            logger.info("Will throw an Exception. Action: " + action)
+            raise Exception("Error for client tests")
+        logger.info("Normal work - Unknow action: " + action)
+    else:
+        logger.info("Normal work - All OK!")
+    
+    return {
+        "message": "All OK!",
+        "action": action
+    } 
