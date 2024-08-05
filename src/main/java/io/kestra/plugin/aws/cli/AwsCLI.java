2a93f5e9c72f2a180c0e1d9fc7475321eb505385
Loïc Mathieu <loikeseke@gmail.com>
2024-08-05 19:14:32 +0200

    fix: task runner INSTANCE -> instance()

--- src/main/java/io/kestra/plugin/aws/cli/AwsCLI.java
+++ src/main/java/io/kestra/plugin/aws/cli/AwsCLI.java
@@ -91,7 +91,7 @@ public class AwsCLI extends AbstractConnection implements RunnableTask<ScriptOut
     @PluginProperty
     @Builder.Default
     @Valid
-    private TaskRunner taskRunner = Docker.INSTANCE;
+    private TaskRunner taskRunner = Docker.instance();
 
     @Schema(title = "The task runner container image, only used if the task runner is container-based.")
     @PluginProperty(dynamic = true)
