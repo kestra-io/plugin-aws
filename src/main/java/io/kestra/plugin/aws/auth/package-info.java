dbc60dbda6d0081c77e508c6044746e0978d2701
Mathieu Gabelle <mgabelle@kestra.io>
2024-10-07 11:11:32 +0200

    feat(eks): add eks oauth token retrieval
    retrieve eks token from credentials
    
    add unit test

---
+++ src/main/java/io/kestra/plugin/aws/auth/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    title = "Authentication",
+    description = "This sub-group of plugins contains tasks to manage authentication for AWS.",
+    categories = { PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.aws.auth;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
