b7fc36433b8e829af2c74639f45fc13e5745779e
brian.mulier <bmmulier@hotmail.fr>
2025-02-21 14:16:00 +0100

    fix: add package-info.java to cli package

---
+++ src/main/java/io/kestra/plugin/aws/cli/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks to interact with AWS CLI. ",
+    categories = { PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.aws.cli;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
