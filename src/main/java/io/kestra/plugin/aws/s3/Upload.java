a28401dc1bf276a04ebb0ff82e0343f2597b8aea
Will Russell <wrussell@kestra.io>
2025-03-14 12:53:55 +0000

    fix(docs): s3 upload example

--- src/main/java/io/kestra/plugin/aws/s3/Upload.java
+++ src/main/java/io/kestra/plugin/aws/s3/Upload.java
@@ -39,24 +39,60 @@ import static io.kestra.core.utils.Rethrow.throwFunction;
     examples = {
         @Example(
             full = true,
+            title = "Upload a FILE input to S3",
             code = """
                 id: aws_s3_upload
                 namespace: company.team
 
                 inputs:
+                  - id: bucket
+                    type: STRING
+                    defaults: my-bucket
+            
                   - id: myfile
                     type: FILE
 
                 tasks:
                   - id: upload
                     type: io.kestra.plugin.aws.s3.Upload
-                    accessKeyId: "<access-key>"
-                    secretKeyId: "<secret-key>"
-                    region: "eu-central-1"
+                    region: "{{ secret('AWS_DEFAULT_REGION') }}"
+                    accessKeyId: "{{ secret('AWS_ACCESS_KEY_ID') }}"
+                    secretKeyId: "{{ secret('AWS_SECRET_KEY_ID') }}"
                     from: "{{ inputs.myfile }}"
-                    bucket: "my-bucket"
+                    bucket: "{{ inputs.bucket }}"
                     key: "path/to/file"
                 """
+        ),
+        @Example(
+            full = true,
+            title = "Download a file and upload it to S3",
+            code = """
+                id: upload_file_to_s3
+                namespace: company.team
+                
+                inputs:
+                  - id: bucket
+                    type: STRING
+                    defaults: my-bucket
+                
+                  - id: file_url
+                    type: STRING
+                    defaults: https://wri-dataportal-prod.s3.amazonaws.com/manual/global_power_plant_database_v_1_3.zip
+                
+                tasks:
+                  - id: download_file
+                    type: io.kestra.plugin.core.http.Download
+                    uri: "{{ inputs.file_url }}"
+                
+                  - id: upload_to_s3
+                    type: io.kestra.plugin.aws.s3.Upload
+                    from: "{{ outputs.download_file.uri }}"
+                    key: powerplant/global_power_plant_database.zip
+                    bucket: "{{ inputs.bucket }}"
+                    region: "{{ secret('AWS_DEFAULT_REGION') }}"
+                    accessKeyId: "{{ secret('AWS_ACCESS_KEY_ID') }}"
+                    secretKeyId: "{{ secret('AWS_SECRET_KEY_ID') }}"
+            """
         )
     }
 )
