669eb6267d58847ba5af762482b2000723fd3c6f
Divanshu Prajapat <honex@Divanshus-MacBook-Air.local>
2025-12-12 18:17:37 +0530

    chore(s3): remove unused toHeaderValue() and clean up SSE enum

--- src/main/java/io/kestra/plugin/aws/s3/models/S3ServerSideEncryption.java
+++ src/main/java/io/kestra/plugin/aws/s3/models/S3ServerSideEncryption.java
@@ -4,12 +4,4 @@ public enum S3ServerSideEncryption {
     NONE,
     AES256,
     AWS_KMS;
-
-    public String toHeaderValue() {
-        return switch (this) {
-            case AES256 -> "AES256";
-            case AWS_KMS -> "aws:kms";
-            default -> null;
-        };
-    }
 }
