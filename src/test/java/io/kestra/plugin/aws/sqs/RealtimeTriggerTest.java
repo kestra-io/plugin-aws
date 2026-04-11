63dfd7524b7785dad957916676b89556b89eb40a
Malay Dewangan <malaydewangan310@gmail.com>
2026-04-12 00:55:42 +0530

    fix: v2 compatibility

--- src/test/java/io/kestra/plugin/aws/sqs/RealtimeTriggerTest.java
+++ src/test/java/io/kestra/plugin/aws/sqs/RealtimeTriggerTest.java
@@ -2,67 +2,94 @@ package io.kestra.plugin.aws.sqs;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Test;
 import org.testcontainers.containers.localstack.LocalStackContainer;
 
-import io.kestra.core.junit.annotations.KestraTest;
 import io.kestra.core.models.executions.Execution;
 import io.kestra.core.models.property.Property;
-import io.kestra.core.queues.QueueFactoryInterface;
-import io.kestra.core.queues.QueueInterface;
+import io.kestra.core.queues.DispatchQueueInterface;
 import io.kestra.core.repositories.LocalFlowRepositoryLoader;
-import io.kestra.core.utils.TestsUtils;
+import io.kestra.core.runners.FlowListeners;
+import io.kestra.jdbc.runner.JdbcScheduler;
 import io.kestra.plugin.aws.sqs.model.Message;
+import io.kestra.scheduler.AbstractScheduler;
+import io.kestra.worker.DefaultWorker;
+
+import io.micronaut.context.ApplicationContext;
 import jakarta.inject.Inject;
-import jakarta.inject.Named;
-import reactor.core.publisher.Flux;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
-@KestraTest(startRunner = true, startScheduler = true)
 class RealtimeTriggerTest extends AbstractSqsTest {
     @Inject
-    @Named(QueueFactoryInterface.EXECUTION_NAMED)
-    private QueueInterface<Execution> executionQueue;
+    private ApplicationContext applicationContext;
+
+    @Inject
+    private FlowListeners flowListenersService;
+
+    @Inject
+    private DispatchQueueInterface<Execution> executionQueue;
 
     @Inject
     protected LocalFlowRepositoryLoader repositoryLoader;
 
     @Test
     void flow() throws Exception {
+        // mock flow listeners
         CountDownLatch queueCount = new CountDownLatch(1);
-        Flux<Execution> receive = TestsUtils.receive(executionQueue, execution -> {
-            queueCount.countDown();
-            assertThat(execution.getLeft().getFlowId(), is("realtime"));
-        });
 
-        repositoryLoader.load(Objects.requireNonNull(RealtimeTriggerTest.class.getClassLoader().getResource("flows/sqs/realtime.yaml")));
-
-        Publish task = Publish.builder()
-            .endpointOverride(Property.ofValue(localstack.getEndpointOverride(LocalStackContainer.Service.SQS).toString()))
-            .queueUrl(Property.ofValue(queueUrl()))
-            .region(Property.ofValue(localstack.getRegion()))
-            .accessKeyId(Property.ofValue(localstack.getAccessKey()))
-            .secretKeyId(Property.ofValue(localstack.getSecretKey()))
-            .from(
-                List.of(
-                    Message.builder().data("Hello World").build()
-                )
+        // scheduler
+        DefaultWorker worker = applicationContext.createBean(DefaultWorker.class, UUID.randomUUID().toString(), 8, null);
+        try (
+            AbstractScheduler scheduler = new JdbcScheduler(
+                this.applicationContext,
+                this.flowListenersService
             )
-            .build();
+        ) {
+            // wait for execution
+            AtomicReference<Execution> lastExecution = new AtomicReference<>();
+            executionQueue.addListener(execution -> {
+                lastExecution.set(execution);
+                queueCount.countDown();
+                assertThat(execution.getFlowId(), is("realtime"));
+            });
 
-        task.run(runContextFactory.of());
+            worker.run();
+            scheduler.run();
 
-        boolean await = queueCount.await(1, TimeUnit.MINUTES);
-        assertThat(await, is(true));
+            repositoryLoader.load(Objects.requireNonNull(RealtimeTriggerTest.class.getClassLoader().getResource("flows/sqs/realtime.yaml")));
 
-        Execution last = receive.blockLast();
-        assertThat(last.getTrigger().getVariables().size(), is(1));
-        assertThat(last.getTrigger().getVariables().get("data"), is("Hello World"));
+            // publish two messages to trigger the flow
+            Publish task = Publish.builder()
+                .endpointOverride(Property.ofValue(localstack.getEndpointOverride(LocalStackContainer.Service.SQS).toString()))
+                .queueUrl(Property.ofValue(queueUrl()))
+                .region(Property.ofValue(localstack.getRegion()))
+                .accessKeyId(Property.ofValue(localstack.getAccessKey()))
+                .secretKeyId(Property.ofValue(localstack.getSecretKey()))
+                .from(
+                    List.of(
+                        Message.builder().data("Hello World").build()
+                    )
+                )
+                .build();
+
+            var runContext = runContextFactory.of();
+
+            task.run(runContext);
+
+            boolean await = queueCount.await(1, TimeUnit.MINUTES);
+            assertThat(await, is(true));
+
+            Execution last = lastExecution.get();
+            assertThat(last.getTrigger().getVariables().size(), is(1));
+            assertThat(last.getTrigger().getVariables().get("data"), is("Hello World"));
+        }
     }
 
 }
